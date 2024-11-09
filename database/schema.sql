0096f59cf128c35e25395da7c05fcd99d852a902
Zoobia Syyed <syyedzoobia@gmail.com>
2024-11-09 00:40:52 +0000

    still setting up, need to insert info into database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,33 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "products" (
+  "productId" serial PRIMARY KEY,
+  "name" text,
+  "category" text,
+  "price" integer,
+  "description" text
+);
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "username" text UNIQUE,
+  "hashedPassword" text NOT NULL
+);
+
+CREATE TABLE "cart" (
+  "cartId" serial PRIMARY KEY,
+  "userId" integer NOT NULL
+);
+
+CREATE TABLE "cartItems" (
+  "cartItemId" serial PRIMARY KEY,
+  "cartId" integer NOT NULL,
+  "productId" integer NOT NULL,
+  "quantity" integer DEFAULT 0
+);
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "cartItems" ADD FOREIGN KEY ("cartId") REFERENCES "cart" ("cartId");
